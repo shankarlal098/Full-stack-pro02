8b3dd4be5a895011a7968a87a6ed7d4074b6391c
Shankar lal <cleet1304@gmail.com>
2026-08-17 17:38:14 +0530

    fixed url

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'https://leetcode-backend-app.onrender.com',
+    baseURL: 'https://backcode-t4o3.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
